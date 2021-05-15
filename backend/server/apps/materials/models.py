f2dfb978b718a0a34a71b47c430fa6da79f67968
Valeria Starkova <stavaser@icloud.com>
2021-05-15 18:38:02 +0900

    added course models

--- backend/server/apps/materials/models.py
+++ backend/server/apps/materials/models.py
@@ -21,13 +21,32 @@ class VacancyRequirements(models.Model):
     description = models.TextField(
         null=True, blank=True, db_column="description", verbose_name="Описание", )
     
+# модели курсов
 
+class Courses(models.Model):
+    organization = models.ForeignKey(to=Organization,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, db_column="title", verbose_name="Название", )
+    description = models.TextField(null=True, blank=True, db_column="description", verbose_name="Описание", )
+
+class CourseMedia(models.Model):
+    course = models.ForeignKey(to=Courses, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, db_column="title", verbose_name="Название", )
+    description = models.TextField(null=True, blank=True, db_column="description", verbose_name="Описание", )
+    video = models.CharField(max_length=240)
+
+class CourseProgress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(to=Courses, on_delete=models.CASCADE)
+
+class CourseMediaProgress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    CourseMedia = models.ForeignKey(to=CourseMedia, on_delete=models.CASCADE)
 
 # модели проектов
 
 class Projects(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, db_column="title", verbose_name="Название", )
     skill = models.CharField(max_length=64)
     description = models.TextField(
@@ -47,6 +66,7 @@ class Teammates(models.Model):
                              on_delete=models.CASCADE)
     team = models.ForeignKey(to=Teams,
                              on_delete=models.CASCADE)
+
 
 # модели тестов
 
